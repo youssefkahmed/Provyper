6de6454737c77e6a0ca47fc8220571a9973d621f
Youssef Khaled <youssefkahmed@gmail.com>
2021-02-18 12:32:53 +0200

    Removed unnecessary src folder

--- src/screens/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('confirmation', views.confirmation, name='confirmation'),
-    path('request_appointment', views.request_appointment, name='request'),
-]
