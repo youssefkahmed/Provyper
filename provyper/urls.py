dcb6451309ab9a47e6d90e35869eab5af6d80dc5
Youssef Khaled <youssefkahmed@gmail.com>
2021-02-18 13:21:00 +0200

    Fixed URLs by adding trailing '/' to all endpoints.

--- provyper/urls.py
+++ provyper/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('screens.urls')),
+    path('/', include('screens.urls')),
     path('admin/', admin.site.urls)
 ]
--- screens/urls.py
+++ screens/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('confirmation', views.confirmation, name='confirmation'),
-    path('request_appointment', views.request_appointment, name='request'),
+    path('/', views.home, name='home'),
+    path('confirmation/', views.confirmation, name='confirmation'),
+    path('request_appointment/', views.request_appointment, name='request'),
 ]
